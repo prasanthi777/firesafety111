fac9e0b35a18f32d67455f18501cf227ed2ce984
prasanthi777 <114868491+prasanthi777@users.noreply.github.com>
2023-01-20 12:21:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+function loadCoupon() {
+    document.getElementsByClassName('coupon')[0].style.display = "block";
+    document.getElementsByTagName('body').style.opacity = "0.2"
+    }
+
+    function closeCoupon() {
+        document.getElementsByClassName('coupon')[0].style.display = "none";
+        document.getElementsByTagName('body').style.opacity = "1"
+    }
+
+
